d80ff00c136a3bf3f15274f67728da348d8f89fe
Uriel Lara <141281252+UrieLara@users.noreply.github.com>
2025-11-01 13:16:46 -0300

    Enter para avanzar en los diálogos
    Además de dar clic en el botón de "siguiente", también se pueden ir pasando las líneas de los diálogos con Enter

--- ap-2---golden-drop/Scripts/cinematicas/dialogos.gd
+++ ap-2---golden-drop/Scripts/cinematicas/dialogos.gd
@@ -21,10 +21,14 @@ func _ready():
 	$"..".connect("finaliza_cinematica", _iniciar_dialogos)
 	mostrar_dialogo_actual()
 	sonido.play()
-	
-func _input(event):
-	pass
 
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+			siguiente_linea()
+			
+func _on_boton_pressed() -> void:
+	var continua = siguiente_linea()
+	
 func _iniciar_dialogos():
 	cambiar_indice = true				
 
@@ -33,15 +37,17 @@ func mostrar_dialogo_actual():
 					
 func siguiente_linea(): 
 	if cambiar_indice:
-			if indice < dialogos.size()-1:	
-				indice += 1
-				mostrar_dialogo_actual()
-			if indice == dialogos.size()-1:
-				cambiar_indice = false
-						
-	return cambiar_indice
+		print(indice)
+		if indice < dialogos.size()-1:	
+			indice += 1
+			mostrar_dialogo_actual()
+		else:
+			cambiar_indice = false
+			
+		if indice == dialogos.size()-1:
+			cambiar_texto_boton()
 
-func _on_boton_pressed() -> void:
-	var continua = siguiente_linea()
-	if !continua:
-		boton.text = "Empezar"
+func cambiar_texto_boton():
+	boton.text = "Empezar"
+	
+		
