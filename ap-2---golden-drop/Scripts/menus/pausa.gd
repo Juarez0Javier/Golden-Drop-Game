129076c7721ec01c8e3c34ce9f5f205bfe3849a0
BriossoA <71294955+BriossoA@users.noreply.github.com>
2025-11-03 19:34:44 -0300

    Menu Pausa
    Mantener los Canvas Layer en 0 o menos porque interfieren con los botones del menu

---
+++ ap-2---golden-drop/Scripts/menus/pausa.gd
@@ -0,0 +1,50 @@
+extends Control
+
+
+var mainmenu : PackedScene
+
+
+func _ready() -> void:
+	hide()
+	mainmenu = load("res://Escenas/Main.tscn")
+
+func _process(_delta: float) -> void:
+	abrirMenu()
+
+func continuar():
+	get_tree().paused = false
+	for child in get_parent().get_children():
+		if(child is ColorRect):
+			child.hide()
+	hide()
+
+
+func pausar():
+	get_tree().paused = true
+	for child in get_parent().get_children():
+		if(child is ColorRect):
+			child.show()
+	show()
+
+
+func abrirMenu():
+	if Input.is_action_just_pressed("Pausar") and get_tree().paused == false:
+		pausar()
+		
+	elif Input.is_action_just_pressed("Pausar") and get_tree().paused == true:
+		continuar()
+
+
+func _on_continuar_pressed() -> void:
+	continuar()
+
+
+func _on_opciones_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_menu_principal_pressed() -> void:
+	var main = mainmenu.instantiate()
+	self.get_parent().get_parent().get_parent().add_child(main)
+	continuar()
+	self.get_parent().get_parent().call_deferred("queue_free")
